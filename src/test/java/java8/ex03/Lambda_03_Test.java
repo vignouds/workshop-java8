84d3ceab1bb551c71489f830a3a40301711259c5
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-17 17:59:03 +0200

    exercice 3

--- src/test/java/java8/ex03/Lambda_03_Test.java
+++ src/test/java/java8/ex03/Lambda_03_Test.java
@@ -4,6 +4,7 @@ import java8.data.Data;
 import java8.data.Person;
 import org.junit.Test;
 
+import java.util.Iterator;
 import java.util.List;
 
 import static org.hamcrest.Matchers.*;
@@ -23,6 +24,8 @@ public class Lambda_03_Test {
     // tag::forEach[]
     private void forEach(List<Person> source, PersonProcessor processor) {
        // TOD0
+    	for(int i=0;i<source.size();i++)
+    		processor.process(source.get(i));
     }
     // end::forEach[]
 
@@ -36,7 +39,12 @@ public class Lambda_03_Test {
         // TODO vérifier qu'une personne à un prénom qui commence par first
         // TODO vérifier qu'une personne à un nom qui commence par last
         // TODO vérifier qu'une personne à un age > 0
-        PersonProcessor verifyPerson = null;
+        
+        PersonProcessor verifyPerson = p->{
+			assertTrue((p.getFirstname()).startsWith("first"));
+			assertTrue((p.getLastname()).startsWith("last"));
+			assertTrue(p.getAge()>0);
+        };
 
         assertThat(verifyPerson, notNullValue());
 
