c935376b8988231b89d4fb71b25d5aab94730898
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-17 14:03:23 +0200

    exercice 3

--- src/test/java/java8/ex03/Method_03_Test.java
+++ src/test/java/java8/ex03/Method_03_Test.java
@@ -17,13 +17,17 @@ public class Method_03_Test {
     // tag::IDao[]
     interface IDao {
         List<Person> findAll();
-
+        
         // TODO créer une méthode statique IDao getDefaultInstance()
         // TODO cette méthode retourne une instance de la classe DaoA
+        
+        static IDao getDefaultInstance(){
+        	return new DaoA();
+        }
     }
     // end::IDao[]
 
-    class DaoA implements IDao {
+    static class DaoA implements IDao {
 
         List<Person> people = Data.buildPersonList(20);
 
@@ -38,6 +42,7 @@ public class Method_03_Test {
     public void test_getDefaultInstance() throws Exception {
         // TODO invoquer la méthode getDefaultInstance() pour que le test soit passant
         IDao result = null;
+        result = IDao.getDefaultInstance();
 
         assertThat(result.findAll(), hasSize(20));
     }
