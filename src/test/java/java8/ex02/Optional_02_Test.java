8c4ba49b668d8db1cf73d283e6942e4c6fa35153
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-18 15:54:49 +0200

    exercice 2

--- src/test/java/java8/ex02/Optional_02_Test.java
+++ src/test/java/java8/ex02/Optional_02_Test.java
@@ -29,13 +29,13 @@ public class Optional_02_Test {
 
         // TODO encapsuler la valeur jules dans un type Optional
         // TODO utiliser la méthode "of"
-        Optional<Person> julesOpt = null;
+        Optional<Person> julesOpt = Optional.of(jules);
 
         // TODO appliquer la méthode "filter" à julesOpt avec le prédicat "adult"
-        Optional<Person> adultPerson = null;
+        Optional<Person> adultPerson = julesOpt.filter(adult);
 
         // TODO appliquer la méthode "filter" à julesOpt avec le prédicat "aged"
-        Optional<Person> agedPerson = null;
+        Optional<Person> agedPerson = julesOpt.filter(aged);
 
         assertThat(adultPerson.isPresent(), is(true));
         assertThat(agedPerson.isPresent(), is(false));
@@ -47,10 +47,10 @@ public class Optional_02_Test {
 
         // TODO encapsuler la valeur jules dans un type Optional
         // TODO utiliser la méthode "of"
-        Optional<Person> julesOpt = null;
+        Optional<Person> julesOpt = Optional.of(jules);
 
         // TODO récupérer l'age de jules via la méthode "map"
-        Optional<Integer> julesAge = null;
+        Optional<Integer> julesAge = julesOpt.map(e->e.getAge());
 
         assertThat(julesAge.isPresent(), is(true));
         assertThat(julesAge.get(), is(30));
@@ -63,11 +63,14 @@ public class Optional_02_Test {
 
         // TODO encapsuler la valeur jules dans un type Optional
         // TODO utiliser la méthode "of"
-        Optional<Person> julesOpt = null;
+        Optional<Person> julesOpt = Optional.of(jules);
 
         // TODO appliquer la méthode "filter" à julesOpt avec le prédicat "adult"
         // TODO chaîner avec la méthode "map" pour récupérer l'age
         // TODO utiliser la méthode isPresent pour vérifier que l'age est bien 30, déclencher l'exception GoodException pour valider que la fonction en paramètre de ifPresent a bien été exécutée.
-        // julesOpt.filter...;
+        julesOpt.filter(adult).map(e->e.getAge()).ifPresent(e->{
+        	GoodException ex = new GoodException();
+        	throw ex;
+        });
     }
 }
