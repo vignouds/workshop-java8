7215fe31fd2c4b0b7cc63f6ac593e3662ddfce8f
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-18 12:20:32 +0200

    exercice3

--- src/test/java/java8/ex03/Function_03_Test.java
+++ src/test/java/java8/ex03/Function_03_Test.java
@@ -14,12 +14,15 @@ import static org.junit.Assert.*;
 public class Function_03_Test {
 
     //  tag::makeAChild[]
-    // TODO Compléter la fonction makeAChild
-    // TODO l'enfant possède le nom du père
-    // TODO l'enfant possède le prenom "<PRENOM_PERE> <PRENOM_MERE>"
-    // TODO l'age de l'enfant est 0
-    // TODO le mot de passe de l'enfant est null
-    BinaryOperator<Person> makeAChild = null;
+
+    BinaryOperator<Person> makeAChild = (pere, mere)->{
+    	Person enfant = new Person();
+    	enfant.setLastname(pere.getLastname());
+    	enfant.setFirstname(pere.getFirstname()+" "+mere.getFirstname());
+    	enfant.setAge(0);
+    	enfant.setPassword(null);
+    	return enfant;
+    };
     //  end::makeAChild[]
 
 
@@ -29,8 +32,7 @@ public class Function_03_Test {
         Person father = new Person("John", "France", 25, "johndoe");
         Person mother = new Person("Aline", "Lebreton", 22, "alino");
 
-        // TODO compléter le test pour qu'il soit passant
-        Person child = null;
+        Person child = makeAChild.apply(father, mother);
 
         assertThat(child, hasProperty("firstname", is("John Aline")));
         assertThat(child, hasProperty("lastname", is("France")));
